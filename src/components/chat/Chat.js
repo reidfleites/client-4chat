dd655393b9963ee58716451240f5011795fbe5af
reidfleites <dreinier86@gmail.com>
2022-02-15 15:38:08 +0100

    update70

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -36,7 +36,7 @@ function Chat() {
   const [showPicker, setShowPicker] = useState(false);
   const [notification, setNotification] = useState(false);
   const [roomNotification, setRoomNotification] = useState(false);
-  //const [allUsers, setAllUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
   const socket = useRef();
   const scrollRef = useRef();
   const [rooms, setRooms] = useState([]);
@@ -71,13 +71,12 @@ function Chat() {
     );
     if (response.ok) {
       const users = await response.json();
-      return users;
-     // setAllUsers([...users]);
+      setAllUsers([...users]);
     }
   };
   //return User from  allUser
   const getUser = async(id) => {
-    const allUsers = await getAllUsers();
+     await getAllUsers();
     if (allUsers.length > 0) {
       const user = allUsers.find((user) => user._id === id);
       return user.username;
@@ -85,7 +84,7 @@ function Chat() {
   };
   //retun avatar number
   const getAvatar = async(id) => {
-    const allUsers=await getAllUsers();
+    await getAllUsers();
     if (allUsers > 0) {
       const user = allUsers.find((user) => user._id === id);
       return user.avatar;
