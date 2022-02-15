b8ecb89b4942071270d7ec13316926c0fd8649dd
reidfleites <dreinier86@gmail.com>
2022-02-15 11:28:21 +0100

    comment

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -8,6 +8,7 @@ import { FaRegSmile } from "react-icons/fa";
 import { FiSend } from "react-icons/fi";
 import Picker from "emoji-picker-react";
 //import { BiMessageRounded } from "react-icons/bi";
+//coment für deploy
 import logo from "../../images/Logo.png";
 import { Avatar } from "@material-ui/core";
 import useSound from "use-sound";
