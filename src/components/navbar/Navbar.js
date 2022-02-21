75d003946c1df3c97e7306cfa9c9043349afe2b4
saeideh-fasih <saeideh.fasih@gmail.com>
2022-02-21 18:22:29 +0100

    update darkmode

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import { AppContext } from "../context/AppContext";
+import logo from "../../images/Logo.png";
 
 function Navbar() {
   const [currentUser] = useContext(AppContext);
@@ -9,18 +10,25 @@ function Navbar() {
   return (
     <div className="navbar">
       {currentUser.username === "" && (
-        <ul>
-          <li>
-            <NavLink to="/" className="inActive" activeClassName="active">
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="signup" className="inActive" activeClassName="active">
-              Signup
-            </NavLink>
-          </li>
-        </ul>
+        <>
+          <img className="logo" src={logo} alt="Logo" />
+          <ul>
+            <li>
+              <NavLink to="/" className="inActive" activeClassName="active">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="signup"
+                className="inActive"
+                activeClassName="active"
+              >
+                Signup
+              </NavLink>
+            </li>
+          </ul>
+        </>
       )}
     </div>
   );
